5e28cb17d9ff21979aba6ff2e179909f3d55175e
Zee Vasilyev <zeejfps@gmail.com>
2025-03-14 16:06:09 -0500

    Added ResourcesCapabilityModel

---
+++ Protocol/Models/ServerCapabilities/ResourcesCapabilityModel.cs
@@ -0,0 +1,25 @@
+﻿namespace McpSdk.Protocol.Models.ServerCapabilities;
+
+public sealed class ResourcesCapabilityModel
+{
+    public bool? IsResourceChangedNotificationSupported { get; set; }
+    public bool? IsListChangedNotificationSupported { get; set; }
+
+    public ResourcesCapabilityModel()
+    {
+    }
+        
+    public ResourcesCapabilityModel(IJsonObject jsonObject)
+    {
+        IsListChangedNotificationSupported = jsonObject["listChanged"]?.AsBool() ?? false;   
+    }
+
+    public void AsJson(IJsonWriter writer)
+    {
+        if (IsResourceChangedNotificationSupported.HasValue)
+            writer.Write("subscribe", IsResourceChangedNotificationSupported.Value);
+        
+        if (IsListChangedNotificationSupported.HasValue) 
+            writer.Write("listChanged", IsListChangedNotificationSupported.Value);
+    }
+}
--- Protocol/Models/ServerCapabilities/ServerCapabilitiesModel.cs
+++ Protocol/Models/ServerCapabilities/ServerCapabilitiesModel.cs
@@ -4,6 +4,7 @@
     {
         public ToolsCapabilityModel Tools { get; set; }
         public PromptsCapabilityModel Prompts { get; set; }
+        public ResourcesCapabilityModel Resources { get; set; }
 
         public ServerCapabilitiesModel() {}
         
@@ -12,12 +13,26 @@
             var toolsObj = jsonObject["tools"]?.AsObject();
             if (toolsObj != null)
                 Tools = new ToolsCapabilityModel(toolsObj);
+            
+            var promptsObj = jsonObject["prompts"]?.AsObject();
+            if (promptsObj != null)
+                Prompts = new PromptsCapabilityModel(promptsObj);
+            
+            var resourcesObj = jsonObject["resources"]?.AsObject();
+            if (resourcesObj != null)
+                Resources = new ResourcesCapabilityModel(resourcesObj);
         }
         
         public void AsJson(IJsonWriter writer)
         {
             if (Tools != null)
                 writer.Write("tools", Tools.AsJson);
+            
+            if (Prompts != null)
+                writer.Write("prompts", Prompts.AsJson);
+            
+            if (Resources != null)
+                writer.Write("resources", Resources.AsJson);
         }
     }
 }
