292e6115911d9df89c38808af2a009f151bb5123
Zee Vasilyev <zeejfps@gmail.com>
2026-06-28 11:20:46 -0500

    Some more cleanup

--- Client/Transports/StdioTransport.cs
+++ Client/Transports/StdioTransport.cs
@@ -118,4 +118,33 @@ namespace McpSdk.Client
             }
         }
     }
+    
+    public sealed class StdioTransportFactory : ITransportFactory
+    {
+        private readonly IJson _json;
+        private readonly string _command;
+        private readonly string _arguments;
+
+        public StdioTransportFactory(IJson json, string command, string[] args)
+        {
+            _json = json;
+            _command = command;
+            _arguments = string.Join(" ", args);
+        }
+
+        public ITransport Create(ILoggerFactory loggerFactory)
+        {
+            return new StdioTransport(_command, _arguments, _json, loggerFactory);
+        }
+    }
+    
+    public static class StdioTransportClientBuilderExtensions
+    {
+        public static ClientBuilder WithStdioTransport(this ClientBuilder clientBuilder, IJson json, string command, string[] args)
+        {
+            var factory = new StdioTransportFactory(json, command, args);
+            clientBuilder.WithTransport(factory);
+            return clientBuilder;
+        }
+    }
 }
